648c0f5560398c40937e6cda41f163b99fe3aa9b
quockhang2030 <quockhag2003@gmail.com>
2023-12-10 18:55:01 +0700

    Initial solution and repository

---
+++ eCommerceSolution.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace eCommerceSolution.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
